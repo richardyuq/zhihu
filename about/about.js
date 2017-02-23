f76d5c667381c857dfb1331115398cc280fc8ba5
{虞青}({021147}) <richard.yuq@alibaba-inc.com>
2017-02-23 19:30:06 +0800

    aaaaaa

---
+++ about/about.js
@@ -0,0 +1,50 @@
+/**
+ * Class Entry for zhihu
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { NavBar, List, Icon, Flex, WhiteSpace } from 'antd-mobile';
+
+import "./about.css";
+
+const Item = List.Item;
+
+let Main = React.createClass({
+	
+	getInitialState() {
+		return {
+
+		}
+	},
+	
+	render() {	
+		const PlaceHolder = (className,content) => (
+				  <div className={className}>
+				  	{content}
+				  </div>
+				);
+	    return (
+		    <div>
+		      	<div style={{ height: 8 }} />
+				<NavBar leftContent="返回" mode="light" onLeftClick={() => console.log('onLeftClick')}>关于我们</NavBar>
+				<div className="flex-container">
+					<Flex justify="center">
+			          {PlaceHolder('inline',<img src='http://7xoh8w.com1.z0.glb.clouddn.com/zhuhu-logo.png'/>)}
+			          {PlaceHolder('inline2',<div style={{textAlign:'left'}}>「悦问」是一个为教育而生的轻问答平台，旨在帮助用户以最便捷的方式获得知识经验和针对性解决方法。悦问好答，分享经验，解决困惑，协同成长。</div>)}
+			        </Flex>
+				</div>
+				<list>
+					<Item wrap>欢迎页</Item>
+					<Item wrap>版权信息</Item>
+				</list>
+				<div className="flex-container">
+					<Flex justify="center" wrap="wrap">
+						{PlaceHolder('inline2',<span>Copyright ©2016-2017<br/>杭州葳锐信息科技有限公司</span>)}
+			        </Flex>
+		        </div>
+			</div>
+	    );
+	}
+});
+
+ReactDOM.render(<Main />, document.getElementById('app'));
---
+++ pay/pay.js
@@ -0,0 +1,57 @@
+/**
+ * Class Entry for zhihu
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Checkbox, NavBar, List, Icon, Flex, WhiteSpace } from 'antd-mobile';
+
+import "./pay.css";
+
+const Item = List.Item;
+
+let Main = React.createClass({
+	
+	getInitialState() {
+		return {
+
+		}
+	},
+	
+	render() {	
+		const PlaceHolder = (className,content) => (
+				  <div className={className}>
+				  	{content}
+				  </div>
+				);
+	    return (
+		    <div>
+		      	<div style={{ height: 8 }} />
+				<NavBar leftContent="返回" mode="light" onLeftClick={() => console.log('onLeftClick')}>确认交易</NavBar>
+				<div className="flex-container">
+					<Flex justify="center" wrap="wrap">
+						{PlaceHolder('inline2',<div>在「悦问」收听steven的回答，需支付：</div>)}
+						{PlaceHolder('inline2',<div style={{color:'red',fontWeight:'bold',fontSize:'larger'}}>￥1.00</div>)}
+			        </Flex>
+			        <WhiteSpace/>
+			        <Flex>
+						<Flex.Item>{PlaceHolder('inline',<div>选择支付方式：</div>)}</Flex.Item>
+					</Flex>
+		        </div>
+				<list>
+					<Item wrap>
+						<div style={{float:'left',width:'90%'}}>
+							<img src='http://7xoh8w.com1.z0.glb.clouddn.com/%E7%BB%BF%E8%89%B2logo.png'/>
+							<span style={{paddingLeft:'10px'}}>微信支付</span>
+						</div>	
+						<div style={{float:'right',width:'10%'}}>
+							<Checkbox></Checkbox>
+						</div>
+					</Item>
+					<Item wrap>其他支付方式</Item>
+				</list>
+			</div>
+	    );
+	}
+});
+
+ReactDOM.render(<Main />, document.getElementById('app'));
